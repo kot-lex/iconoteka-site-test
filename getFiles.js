23bcaf912bcb2f40bc203070edaecebee426858e
Aleksei Chernov <kot.lex@gmail.com>
2018-07-03 00:23:33 +0300

    Generate json catalogue by given dir

---
+++ getFiles.js
@@ -0,0 +1,28 @@
+const fse = require('fs-extra');
+const path = require('path');
+
+function getIconObject(filename) {
+    const parts = filename.split('_');
+    
+}
+
+async function getFiles(dir) {
+    const dirName = dir.split('/').reverse()[0];
+    const contents = await fse.readdir(dir);
+
+    const data = contents.map(async item => {
+        const stats = await fse.stat(`${dir}/${item}`);
+        if (stats.isDirectory()) {
+            return getFiles(`${dir}/${item}`);
+        } else {
+            return Promise.resolve(`${dirName}/${item}`);
+        }
+    });
+    return {
+        name: dirName,
+        items: await Promise.all(data)
+    };
+}
+
+getFiles('src/iconoteka')
+    .then(files => fse.writeFileSync('src/iconoteka/iconoteka.json', JSON.stringify(files, null, 2)));
