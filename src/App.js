bcbccd1702371070a9ef4fef342f6d7a9ac3afb8
Aleksei Chernov <kot.lex@gmail.com>
2018-07-02 07:54:06 +0300

    Add sample icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import icon from './iconoteka/Arrows/iconoteka_3d_rotation_b_s.svg';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <img src={icon} style={{ width: '100px', height: '100px' }} />
         </p>
       </div>
     );
