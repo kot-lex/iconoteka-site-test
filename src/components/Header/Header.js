37318b480062047d22753e0749e511123caa7121
Aleksei Chernov <kot.lex@gmail.com>
2018-08-29 02:07:18 +0300

    Add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Header.css';
+import logo from './logo.svg';
 
 export default function Header(props) {
   return (
-    <div className="header" />
+    <div className="header">
+      <img src={logo} className="header__logo" alt="iconoteka"/>
+      <ul className="header__menu">
+        <li><a href="#link">about</a></li>
+        <li><a href="#link">#iconoteka</a></li>
+        <li><a href="#link">github</a></li>
+      </ul>
+    </div>
   );
 }
