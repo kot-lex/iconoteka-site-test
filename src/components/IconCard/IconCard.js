f505dbc60bd04d5022c981a664937b6f523b3a5b
Aleksei Chernov <kot.lex@gmail.com>
2018-08-30 22:47:11 +0300

    Make IconCard clickable

--- src/components/IconCard/IconCard.js
+++ src/components/IconCard/IconCard.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './IconCard.css';
 
-export default function IconCard(props) {
-  const icon = require(`../../iconoteka/${props.path}`);
-  return !props.isHidden && (
-  <div className="icon-card">
-    <img className="icon-card__icon" src={props.baseUrl + icon} alt="icon" />
-    <span className="icon-card__title">{props.name}</span>
-    <a className="icon-card__download" download={props.path} href={props.baseUrl + icon}>Download</a>
-  </div>
-  );
+
+export default class IconCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.linkRef = React.createRef();
+    this.gotoLink = this.gotoLink.bind(this);
+  }
+
+  gotoLink(event) {
+    if (event.target.tagName.toLocaleLowerCase() === 'a') {
+      return;
+    }
+
+    this.linkRef.current.click();
+  }
+
+  render() {
+    const {
+      isHidden, baseUrl, name, path,
+    } = this.props;
+    // eslint-disable-next-line
+    const icon = require(`../../iconoteka/${path}`);
+    const iconAddress = baseUrl + icon;
+
+    return !isHidden && (
+      <div className="icon-card" onClick={this.gotoLink}>
+        <img className="icon-card__icon" src={baseUrl + icon} alt="icon" />
+        <span className="icon-card__title">{name}</span>
+        <a className="icon-card__download" ref={this.linkRef} download={path} href={iconAddress}>Download</a>
+      </div>
+    );
+  }
 }
+
+IconCard.propTypes = {
+  isHidden: PropTypes.bool,
+  baseUrl: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+};
+
+IconCard.defaultProps = {
+  isHidden: false,
+};
