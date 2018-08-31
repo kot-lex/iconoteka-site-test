c4422e336946e1219a27b1a7444abf2de52dfc99
Aleksei Chernov <kot.lex@gmail.com>
2018-09-01 00:24:02 +0300

    Add link to github account

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Header.css';
 import logo from './logo.svg';
 
-export default function Header(props) {
+export default function Header() {
   return (
     <div className="header">
-      <img src={logo} className="header__logo" alt="iconoteka"/>
+      <img src={logo} className="header__logo" alt="iconoteka" />
       <ul className="header__menu">
         <li><a href="#link">about</a></li>
         <li><a href="#link">#iconoteka</a></li>
-        <li><a href="#link">github</a></li>
+        <li><a href="https://github.com/iconoteka/iconoteka">github</a></li>
       </ul>
     </div>
   );
