dc9e0552470e49299a553d5f3c827dedebd09bec
Aleksei Chernov <kot.lex@gmail.com>
2018-09-02 03:34:30 +0300

    Set focus by default

--- src/components/IconsFilter/IconsFilter.js
+++ src/components/IconsFilter/IconsFilter.js
@@ -1,31 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './IconsFilter.css';
 
-export default function IconsFilter(props) {
-  return (
-    <div className="icons-filter">
-      <input className="icons-filter__search" onChange={props.onChange} onKeyUp={props.onChange} placeholder="Search" />
-      <ul className="icons-filter__style">
-        <li>
-                  Style
-        </li>
-        <li className={
-                  props.style === 'fill'
-                    ? 'icons-filter__control icons-filter__control_active'
-                    : 'icons-filter__control'
-              }
-        >
-          <a href="#fill" onClick={event => props.onStyleChange(event, 'fill')}>Fill</a>
-        </li>
-        <li className={
-                  props.style === 'stroke'
-                    ? 'icons-filter__control icons-filter__control_active'
-                    : 'icons-filter__control'
-              }
-        >
-          <a href="#stroke" onClick={event => props.onStyleChange(event, 'stroke')}>Stroke</a>
-        </li>
-      </ul>
-    </div>
-  );
+export default class IconsFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputRef.current.focus();
+  }
+
+  render() {
+    const { onChange, style, onStyleChange } = this.props;
+    return (
+      <div className="icons-filter">
+        <input
+          className="icons-filter__search"
+          onChange={onChange}
+          onKeyUp={onChange}
+          placeholder="Search"
+          ref={this.inputRef}
+        />
+
+        <ul className="icons-filter__style">
+          <li>
+            Style
+          </li>
+          <li className={
+            style === 'fill'
+              ? 'icons-filter__control icons-filter__control_active'
+              : 'icons-filter__control'
+          }
+          >
+            <a href="#fill" onClick={event => onStyleChange(event, 'fill')}>Fill</a>
+          </li>
+          <li className={
+            style === 'stroke'
+              ? 'icons-filter__control icons-filter__control_active'
+              : 'icons-filter__control'
+          }
+          >
+            <a href="#stroke" onClick={event => onStyleChange(event, 'stroke')}>Stroke</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+IconsFilter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onStyleChange: PropTypes.func.isRequired,
+  style: PropTypes.oneOf(['fill', 'stroke']).isRequired,
+};
