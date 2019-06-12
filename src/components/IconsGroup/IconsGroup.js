618253df20d0e3e32734926a71e13e3e6b4798bb
Aleksei Chernov <kot.lex@gmail.com>
2019-06-12 16:36:43 +0300

    Load visible icons only

--- src/components/IconsGroup/IconsGroup.js
+++ src/components/IconsGroup/IconsGroup.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Waypoint } from 'react-waypoint';
 
 import IconCard from '../IconCard';
 import './IconsGroup.scss';
 
+const IconCardWithRef = React.forwardRef((props, ref) => {
+  return <IconCard innerRef={ref} {...props} />;
+});
+
 export default function IconsGroup({ group, baseUrl }) {
+  const [isVisible, setIsVisible] = useState(false);
+
   const images = group.items
     .map(iconItem => (
-      <IconCard
-        key={iconItem.fileName}
-        path={iconItem.path}
-        name={iconItem.name}
-        fileName={iconItem.fileName}
-        isHidden={iconItem.isHidden}
-        baseUrl={baseUrl}
-      />
+      <Waypoint
+        onEnter={() => setIsVisible(true)}
+      >
+        <IconCardWithRef
+          key={iconItem.fileName}
+          path={iconItem.path}
+          name={iconItem.name}
+          fileName={iconItem.fileName}
+          isHidden={iconItem.isHidden}
+          baseUrl={baseUrl}
+          isVisible={isVisible}
+        />
+      </Waypoint>
     ));
 
   return (
