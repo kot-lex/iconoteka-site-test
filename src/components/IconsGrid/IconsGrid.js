fe050ff50f15b3be3e44ab6d179622189526a131
Aleksei Chernov <kot.lex@gmail.com>
2019-06-16 20:11:20 +0300

    Add empty text

--- src/components/IconsGrid/IconsGrid.js
+++ src/components/IconsGrid/IconsGrid.js
@@ -14,7 +14,11 @@ export default function IconsGrid({ items, baseUrl }) {
     : (
       <div className="icons-grid">
         <div className="icons-group">
-          <h3 className="icons-group__title">We don&#39;t have this icon yet</h3>
+          <h3 className="icons-group__title icons-group__title_empty">Sorry, we don't have this icon yet. <br />
+You can submit an icon and we will add it to the production queue.
+
+</h3>
+        <a href="mailto:hello@iconoteka.com" className="icons-group__submit_icon">Submit icon</a>
         </div>
       </div>
     );
