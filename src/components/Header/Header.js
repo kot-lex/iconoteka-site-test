80fead544d89318ad07cac01c31efa2731117fa2
Aleksei Chernov <kot.lex@gmail.com>
2019-06-16 23:09:07 +0300

    Add patreon link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
       <img src={logo} className="header__logo" alt="iconoteka" />
       <ul className="header__menu">
         <li><a href="https://readymag.com/turbaba/1296248/">about</a></li>
-        <li><a href="#link">support project</a></li>
+        <li><a href="https://www.patreon.com/iconoteka">support project</a></li>
       </ul>
     </header>
 
